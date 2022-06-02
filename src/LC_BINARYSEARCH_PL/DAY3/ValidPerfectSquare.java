de5b1de8d579b86168a4a9201050f23a33498189
adityaraj <developer.rajaditya@gmail.com>
2022-06-02 16:04:21 +0530

    BINARY SEARCH PRACTICE DAY3.

---
+++ src/LC_BINARYSEARCH_PL/DAY3/ValidPerfectSquare.java
@@ -0,0 +1,31 @@
+package LC_BINARYSEARCH_PL.DAY3;
+// QUESTION 367. Valid Perfect Square
+// https://leetcode.com/problems/valid-perfect-square/
+
+public class ValidPerfectSquare {
+//    SOLUTION :
+    public static boolean isPerfectSquare(int num) {
+        if(num == 1){
+            return true;
+        }
+        long start = 0;
+        long end = num/2;
+        while(start <= end){
+            long mid = start + (end - start)/2;
+            long temp = mid*mid;
+            if(temp == num){
+                return true;
+            }else if(temp < num){
+                start = mid + 1;
+            }else{
+                end = mid - 1;
+            }
+        }
+        return false;
+    }
+
+//    HELPER FUNCTION :
+//    public static void main(String[] args) {
+//        System.out.println(isPerfectSquare(1));
+//    }
+}
