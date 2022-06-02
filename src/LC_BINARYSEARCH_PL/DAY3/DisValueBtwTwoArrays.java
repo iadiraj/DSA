326c96cb49055be92e011dd4994928a018400bc6
adityaraj <developer.rajaditya@gmail.com>
2022-06-02 16:05:37 +0530

    BINARY SEARCH PRACTICE DAY3.

---
+++ src/LC_BINARYSEARCH_PL/DAY3/DisValueBtwTwoArrays.java
@@ -0,0 +1,42 @@
+package LC_BINARYSEARCH_PL.DAY3;
+// QUESTION 1385. Find the Distance Value Between Two Arrays
+// https://leetcode.com/problems/find-the-distance-value-between-two-arrays/
+
+import java.util.Arrays;
+
+public class DisValueBtwTwoArrays {
+//    SOLUTION :
+    public static int findTheDistanceValue(int[] arr1, int[] arr2, int d) {
+        Arrays.sort(arr2);
+        int ans = 0;
+
+        for (int x : arr1)
+            if (check(arr2, x - d, x + d))
+                ans++;
+
+        return ans;
+    }
+//  HELPER FUNCTIONS :
+    private static boolean check(int[] arr, int from, int to){
+        int start = 0;
+        int end = arr.length - 1;
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            if(arr[mid] >= from && arr[mid] <= to){
+                return false;
+            }else if(arr[mid] < from){
+                start = mid + 1;
+            }else if((arr[mid] > to)){
+                end = mid - 1;
+            }
+        }
+        return true;
+    }
+
+//    public static void main(String[] args) {
+//        int[] arr1 = {4,5,8};
+//        int[] arr2 = {10,9,1,8};
+//        int d = 2;
+//        System.out.println(findTheDistanceValue(arr1, arr2, d));
+//    }
+}
