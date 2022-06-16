81652d3aa4e7a19e8f3eded22e68bb698d710225
adityaraj <developer.rajaditya@gmail.com>
2022-06-16 15:34:52 +0530

    BINARY SEARCH (MEDIUM)⭐️

--- src/BINARYSEARCH/SearchInSortedRotArrII.java
+++ src/BINARYSEARCH/SearchInSortedRotArrII.java
@@ -11,30 +11,28 @@ public class SearchInSortedRotArrII {
             if(nums[mid] == target){
                 return true;
             }
-            if(nums[start] == nums[mid] && nums[mid] == nums[end]){
-                start++;
-                end--;
-            }
-            if(nums[start] <= nums[mid]){
+            if(nums[start] < nums[mid]){
                 if(nums[start] <= target && target <= nums[mid]){
                     end = mid - 1;
                 }else{
                     start = mid + 1;
                 }
-            }else{
+            }else if(nums[mid] < nums[end]){
                 if(nums[mid] <= target && target <= nums[end]){
                     start = mid + 1;
                 }else{
                     end = mid - 1;
                 }
+            }else{
+                end--;
             }
         }
         return false;
     }
 //    HELPER FUNCTIONS :
-//    public static void main(String[] args) {
-//        int[] nums = {3, 4, 5, 6, 0, 1, 2, 3, 3, 3, 3,};
-//        int target = 7;
-//        System.out.println(search(nums, target));
-//    }
+    public static void main(String[] args) {
+        int[] nums = {2,5,6,0,0,1,2};
+        int target = 3;
+        System.out.println(search(nums, target));
+    }
 }
