90b7b751f26e7ca1a710aedc48f2f5ecb2e67c57
adityaraj <developer.rajaditya@gmail.com>
2022-06-16 16:10:27 +0530

    BINARY SEARCH (MEDIUM)⭐️

--- src/BINARYSEARCH/SearchMinInSortedRotArr.java
+++ src/BINARYSEARCH/SearchMinInSortedRotArr.java
@@ -17,8 +17,8 @@ public class SearchMinInSortedRotArr {
         return nums[start];
     }
 //    HELPER FUNCTIONS :
-    public static void main(String[] args) {
-        int[] nums = {3, 4, 5, 1, 2};
-        System.out.println(findMin(nums));
-    }
+//    public static void main(String[] args) {
+//        int[] nums = {3, 4, 5, 1, 2};
+//        System.out.println(findMin(nums));
+//    }
 }
