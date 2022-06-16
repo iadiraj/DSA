e5c64fca837ef2acbc687ca69db6a122c98b1045
adityaraj <developer.rajaditya@gmail.com>
2022-06-16 15:59:45 +0530

    BINARY SEARCH (MEDIUM)⭐️

--- src/BINARYSEARCH/SearchInSortedRotArrII.java
+++ src/BINARYSEARCH/SearchInSortedRotArrII.java
@@ -11,28 +11,32 @@ public class SearchInSortedRotArrII {
             if(nums[mid] == target){
                 return true;
             }
-            if(nums[start] < nums[mid]){
-                if(nums[start] <= target && target <= nums[mid]){
+            if(nums[start] == nums[mid] && nums[mid] == nums[end]){
+                start = start + 1;
+                end = end - 1;
+            }
+            else if(nums[start] <= nums[mid]){
+                if(nums[start] <= target && target < nums[mid]){
                     end = mid - 1;
                 }else{
                     start = mid + 1;
-                }
-            }else if(nums[mid] < nums[end]){
-                if(nums[mid] <= target && target <= nums[end]){
-                    start = mid + 1;
-                }else{
-                    end = mid - 1;
                 }
             }else{
-                end--;
+                if(nums[mid] < target && target <= nums[end]){
+                    start = mid + 1;
+                }else{
+                    end = mid - 1;
+                }
             }
         }
         return false;
     }
 //    HELPER FUNCTIONS :
     public static void main(String[] args) {
-        int[] nums = {2,5,6,0,0,1,2};
-        int target = 3;
-        System.out.println(search(nums, target));
+        int[][] nums = {{1, 0, 1, 1, 1}, {1,1,1,1,1,1,1,1,1,13,1,1,1,1,1,1,1,1,1,1,1,1},{2,5,6,0,0,1,2},{2,5,6,0,0,1,2}};
+        int[] target = {0, 13, 0, 3};
+        for(int i = 0; i < nums.length; i++){
+            System.out.println(search(nums[i], target[i]));
+        }
     }
 }
